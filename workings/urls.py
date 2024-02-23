5bc427cb59f9040ce96003d88eb121c0f7e27fb3
siddhartha <siddharthpaudel41@gmail.com>
2024-02-23 19:29:39 +0545

    updated urls

--- workings/urls.py
+++ workings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TitleView.as_view(), name='titles'),
-    path('\summary', views.ContentView.as_view(), name='content'),
+    path('summary', views.ContentView.as_view(), name='content'),
+    path('search' , views.SearchView.as_view(), name='search'),
 ]
