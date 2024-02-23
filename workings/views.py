31257a87afeaa77e5f28c869a4bae824a70ce7bd
siddhartha <siddharthpaudel41@gmail.com>
2024-02-23 19:30:50 +0545

    added views

--- workings/views.py
+++ workings/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from workings.helpers.get_news_articles import get_top_news, get_news 
 from workings.helpers import single_summariler, multi_summarizer,extract_keywords
+import textwrap
 
 class TitleView(APIView):
     def get(self, request) :
@@ -13,18 +14,29 @@ class TitleView(APIView):
 
 class ContentView(APIView):
     def post(self, request) :
+
         title_data = request.data["title"]
         # get keywords from title
-        keywords = extract_keywords.extract_keywords(title_data)
+        keywords = extract_keywords.extract_keywords(str(title_data))
         # get news articles related to keywords
-        news_articles = get_news(keywords)
+        # convert the keywords of list to string
+        keywords_string = " ".join(keywords)
+        print(f"this is keywords string {keywords_string}" )
+        news_articles = get_news(keywords_string)
+
+        print('\n')
+        print(news_articles)
 
         # summarize each news articles and put in a list
         summaries = []
 
-        for key, value in news_articles.items() :
+        for key, value in news_articles.items():
             summary = single_summariler.generate_summary(value)
             summaries.append(summary)
+
+        print("list of summaries: ")
+        print('\n')
+        print(summaries)
 
         # generate summary of list of news articles
 
@@ -32,13 +44,23 @@ class ContentView(APIView):
         # append list of summary to a single document seperated by |||||
 
         for summary in summaries :
-            document = document + "||||" + summary
+            try: 
+                document = document + "|||||" + summary[0]['summary_text']
+            except UnboundLocalError:
+                document = summary[0]['summary_text']
         
         # generate summary of document
+        try:
             final_summary = multi_summarizer.generate_summary(document)
-        
+        except:
+            return Response("could not generate summary")
 
-        return Response({
-            "summary" : final_summary
-        })
+        print(final_summary)        
+
+        return Response(final_summary)
     
+class SearchView(APIView):
+    def post(self, request) :
+        query = request.data["query"]
+        titles = get_top_news(query)
+        return Response(titles)
