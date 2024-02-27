e9d9970054fa86558c40d1324a18dc723486eb22
Rohit12344 <55599405+Rohit12344@users.noreply.github.com>
2024-02-27 19:51:56 +0545

    WIP:Project setup

---
+++ app/src/lib/axios-instance.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { BASE_API_URL } from './constants';
+
+const axiosInstance = axios.create({
+  baseURL: `${BASE_API_URL}/company`,
+  headers: {
+    accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+const useAxiosInstance = () => {
+  return axiosInstance
+}
+
+export default useAxiosInstance;
