37da8842cc556bcbf1d67684dacfa557fbb1aa03
siddhartha <siddharthpaudel41@gmail.com>
2024-03-12 15:34:51 +0545

    added category api method

--- workings/urls.py
+++ workings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TitleView.as_view(), name='titles'),
     path('summary', views.ContentView.as_view(), name='content'),
     path('search' , views.SearchView.as_view(), name='search'),
+    path('category' , views.categoryView.as_view(), name='category'),
 ]
--- workings/views.py
+++ workings/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from workings.helpers.get_news_articles import get_top_news, get_news 
 from workings.helpers import single_summariler, multi_summarizer,extract_keywords
-import textwrap
+from workings.helpers.news_data import save_news_to_db, get_news_archive
+from workings.models import News
 
 class TitleView(APIView):
     def get(self, request) :
@@ -18,6 +19,16 @@ class ContentView(APIView):
         try:
             title_data = request.data["title"]
             description_data = request.data["description"]
+            # check if title is in News table
+            if(News.objects.filter(title=title_data).exists()):
+                # get first element with the title
+                print("here")
+                news = News.object.filter(title=title_data).first()
+                #news = News.objects.get(title=title_data)
+                content = news.content
+                print(content)
+                summary = single_summariler.generate_summary(content)
+                return Response(summary)
             if "-" in title_data:
                 title_data = title_data.split(" - ")[0].strip()
             keywords = extract_keywords.extract_keywords(str(title_data))
@@ -68,9 +79,20 @@ class ContentView(APIView):
 class SearchView(APIView):
     def post(self, request) :
         query = request.data["query"]
-        try:
-            category = request.data["category"]
-        except KeyError:
-            category = "general"
-        titles = get_top_news(query,category)
+        titles = get_top_news(query)
+        return Response(titles)
+
+class categoryView(APIView):
+    def post(self, request) :
+        category = request.data["category"]
+
+        response = get_news_archive(category)
+        print(response)
+        #clear db before saving
+        if(News.objects.count() > 0):
+            News.objects.all().delete()
+        # save response to db
+        save_news_to_db(response)
+        # get titles from db
+        titles = News.objects.values_list('title', flat=True) 
         return Response(titles)
